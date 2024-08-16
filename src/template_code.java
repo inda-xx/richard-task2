1442d9a3d8818f9aae95315b1ac6034bcf7c3f0b
Avid <107996421+HotFazz@users.noreply.github.com>
2024-08-16 15:18:41 +0200

    Update template_code.java

--- src/template_code.java
+++ src/template_code.java
@@ -1,87 +1,154 @@
-```java
-    // Player Class
-    class Player {
-        private String name;
-        private int xPosition;
-        private int yPosition;
-        private int score;
 
-        // Constructor
-        public Player(String name, int xPosition, int yPosition, int score) {
-            this.name = name;
-            this.xPosition = xPosition;
-            this.yPosition = yPosition;
-            this.score = score;
-        }
+class Player {
+    private String name;
+    private int xPosition;
+    private int yPosition;
+    private int score;
 
-        // Add Getter and Setter methods
-        // ...
-        
-        // Add methods moveUP, moveDown, moveLeft, moveRight
-        // ...
-        
-        // Add increaseScore method
-        // ...
-        
-        // Add attackEnemy method
-        // ...
-         
-        // Use the main method for testing
-        public static void main(String[] args) {
-            // Use this main method to create Player objects and test properties and methods
-        } // end main method
+    public Player(String name, int xPosition, int yPosition, int score) {
+        this.name = name;
+        this.xPosition = xPosition;
+        this.yPosition = yPosition;
+        this.score = score;
+    }
 
-    } // Player Class end
-    
-    
-    // Enemy Class
-    class Enemy {
-        private String type;
-        private int xPosition;
-        private int yPosition;
-        private int damage;
+    public String getName() {
+        return name;
+    }
 
-        // Constructor
-        public Enemy(String type, int xPosition, int yPosition, int damage) {
-            this.type = type;
-            this.xPosition = xPosition;
-            this.yPosition = yPosition;
-            this.damage = damage;
-        }
+    public void setName(String name) {
+        this.name = name;
+    }
 
-       // Add Getter and Setter methods
-       // ...
-       
-       // Add attackPlayer method
-       // ...
+    public int getXPosition() {
+        return xPosition;
+    }
 
-        public static void main(String[] args) {
-            // Use this main method to create Enemy objects and test properties and methods
-        } // end main method
+    public void setXPosition(int xPosition) {
+        this.xPosition = xPosition;
+    }
 
-    } // Enemy Class end
-    
-    
-    // Game Class
-    class Game {
-        private Player player;
-        private Enemy enemy;
+    public int getYPosition() {
+        return yPosition;
+    }
 
-        // Constructor
-        public Game(Player player, Enemy enemy) {
-            this.player = player;
-            this.enemy = enemy;
-        }
+    public void setYPosition(int yPosition) {
+        this.yPosition = yPosition;
+    }
 
-        // Add Getter and Setter methods
-        // ...
-        
-        // Add startGame method
-        // ...
-        
-        public static void main(String[] args) {
-            // Use this main method to create Game objects and test properties and methods
-        } // end main method
+    public int getScore() {
+        return score;
+    }
 
-    } // Game Class end
-```
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public void moveUp() {
+        yPosition += 1;
+    }
+
+    public void moveDown() {
+        yPosition -= 1;
+    }
+
+    public void moveRight() {
+        xPosition += 1;
+    }
+
+    public void moveLeft() {
+        xPosition -= 1;
+    }
+
+    public void increaseScore(int increment) {
+        score += increment;
+    }
+
+    public void attackEnemy(Enemy enemy) {
+        enemy.setDamage(enemy.getDamage() - 10);
+    }
+}
+
+class Enemy {
+    private String type;
+    private int xPosition;
+    private int yPosition;
+    private int damage;
+
+    public Enemy(String type, int xPosition, int yPosition, int damage) {
+        this.type = type;
+        this.xPosition = xPosition;
+        this.yPosition = yPosition;
+        this.damage = damage;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getXPosition() {
+        return xPosition;
+    }
+
+    public void setXPosition(int xPosition) {
+        this.xPosition = xPosition;
+    }
+
+    public int getYPosition() {
+        return yPosition;
+    }
+
+    public void setYPosition(int yPosition) {
+        this.yPosition = yPosition;
+    }
+
+    public int getDamage() {
+        return damage;
+    }
+
+    public void setDamage(int damage) {
+        this.damage = damage;
+    }
+
+    public void attackPlayer(Player player) {
+        player.setScore(player.getScore() - this.damage);
+    }
+}
+
+class Game {
+    private Player player;
+    private Enemy enemy;
+
+    public Game(Player player, Enemy enemy) {
+        this.player = player;
+        this.enemy = enemy;
+    }
+
+    public Player getPlayer() {
+        return player;
+    }
+
+    public void setPlayer(Player player) {
+        this.player = player;
+    }
+
+    public Enemy getEnemy() {
+        return enemy;
+    }
+
+    public void setEnemy(Enemy enemy) {
+        this.enemy = enemy;
+    }
+
+    public void startGame() {
+        // Example of game start
+        player.moveUp();
+        player.increaseScore(10);
+        player.attackEnemy(enemy);
+        enemy.attackPlayer(player);
+    }
+}
