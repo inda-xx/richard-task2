c5bb15e1120bb79d2ba762cc5a7e75989a84fc4b
Avid <107996421+HotFazz@users.noreply.github.com>
2024-08-23 02:40:15 +0200

    Update template_code.java

--- src/template_code.java
+++ src/template_code.java
@@ -1,4 +1,4 @@
-// Class Definition for CharacterX
+finition for CharacterX
 class CharacterX {
     // Instance Variables
     private String characterName;
@@ -7,7 +7,7 @@ class CharacterX {
     private int health;
     private int strength;
     private int score;
-    
+
     // Constructor
     public CharacterX(String characterName, int xPos, int yPos, int health, int strength) {
         this.characterName = characterName;
@@ -19,41 +19,101 @@ class CharacterX {
     }
     
     // Getters and Setters
-    // Add here...
+    public String getCharacterName() {
+        return characterName;
+    }
+    public void setCharacterName(String characterName) {
+        this.characterName = characterName;
+    }
+    public int getXPos() {
+        return xPos;
+    }
+    public void setXPos(int xPos) {
+        this.xPos = xPos;
+    }
+    public int getYPos() {
+        return yPos;
+    }
+    public void setYPos(int yPos) {
+        this.yPos = yPos;
+    }
+    public int getHealth() {
+        return health;
+    }
+    public void setHealth(int health) {
+        this.health = health;
+    }
+    public int getStrength() {
+        return strength;
+    }
+    public void setStrength(int strength) {
+        this.strength = strength;
+    }
+    public int getScore() {
+        return score;
+    }
+    public void setScore(int score) {
+        this.score = score;
+    }
     
     // Method for moving the character
     public void move(String direction) {
-        // Add code here...
+        switch(direction.toUpperCase()) {
+            case "UP":
+                yPos++;
+                gainPoint();
+                break;
+            case "DOWN":
+                yPos--;
+                gainPoint();
+                break;
+            case "LEFT":
+                xPos--;
+                gainPoint();
+                break;
+            case "RIGHT":
+                xPos++;
+                gainPoint();
+                break;
+            case "STAY":
+                break;
+            default:
+                System.out.println("Invalid direction provided");
+        }
     }
     
     // Method for gaining a point
     public void gainPoint() {
-        // Add code here...
+        score += 1;
     }
     
     // Method for confrontation
     public void confront(VillainY opponent) {
-        // Add code here...
+        if(strength > opponent.getStrength()) {
+            int damage = strength - opponent.getStrength();
+            opponent.setHealth(opponent.getHealth() - damage);
+        } else {
+            int damage = opponent.getStrength() - strength;
+            health -= damage;
+        }
     }
     
     // Method for checking if game is over
     public boolean isGameOver() {
-        // Add code here...
+        return health <= 0;
     }
     
     // Method for demonstrating Variable Shadowing
     public void demonstrateShadowing() {
-        // Add code here...
-    }
-
-    // Gameplay and Testing inside the main method
-    public static void main(String[] args) {
-        // Add code here...
+        int health = 10;
+        System.out.println("Instance health: " + this.health);
+        System.out.println("Local health: " + health);
     }
 }
 
 // Class Definition for VillainY
 class VillainY {
+
     // Instance Variables
     private String characterName;
     private int xPos;
@@ -73,20 +133,76 @@ class VillainY {
     }
     
     // Getters and Setters
-    // Add here...
+    public String getCharacterName() {
+        return characterName;
+    }
+    public void setCharacterName(String characterName) {
+        this.characterName = characterName;
+    }
+    public int getXPos() {
+        return xPos;
+    }
+    public void setXPos(int xPos) {
+        this.xPos = xPos;
+    }
+    public int getYPos() {
+        return yPos;
+    }
+    public void setYPos(int yPos) {
+        this.yPos = yPos;
+    }
+    public int getHealth() {
+        return health;
+    }
+    public void setHealth(int health) {
+        this.health = health;
+    }
+    public int getStrength() {
+        return strength;
+    }
+    public void setStrength(int strength) {
+        this.strength = strength;
+    }
+    public int getScore() {
+        return score;
+    }
+    public void setScore(int score) {
+        this.score = score;
+    }
     
     // Method for moving the character
     public void move(String direction) {
-        // Add code here...
+        switch(direction.toUpperCase()) {
+            case "UP":
+                yPos++;
+                gainPoint();
+                break;
+            case "DOWN":
+                yPos--;
+                gainPoint();
+                break;
+            case "LEFT":
+                xPos--;
+                gainPoint();
+                break;
+            case "RIGHT":
+                xPos++;
+                gainPoint();
+                break;
+            case "STAY":
+                break;
+            default:
+                System.out.println("Invalid direction provided");
+        }
     }
     
     // Method for gaining a point
     public void gainPoint() {
-        // Add code here...
+        score += 1;
     }
     
     // Method for checking if game is over
     public boolean isGameOver() {
-        // Add code here...
+        return health <= 0;
     }
 }
